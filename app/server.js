8441b4f09876ac0f2ec1ad40e12ae097bcc4d41b
wesley cundle <wesley.adam.cundle@hotmail.com>
2022-03-04 00:58:38 -0500

    update

--- app/server.js
+++ app/server.js
@@ -57,7 +57,7 @@ if (process.env.MODE === "production") {
 //code for deployment (end)
 
 //listen on port 3500 (start)
-PORT = process.env.PORT || 3400;
+PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`server connected to 3400`);
